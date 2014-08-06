7e64c7291bab722de20f50a043313e804081dd23
Ivan Fantom <ziv78@yandex.ru>
2014-08-06 15:11:35 +0300

    origin

---
+++ scripts/main.js
@@ -0,0 +1,25 @@
+(function ($) {
+	'use strict';
+
+	function DishViewModel(){
+		var self = this;
+
+		self.Dishes = [
+			{ImageSrc:"holder.js/50x50", Name: "Dish1", Price: "100"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish2", Price: "200"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish3", Price: "300"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish4", Price: "400"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish5", Price: "500"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish6", Price: "600"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish7", Price: "700"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish8", Price: "800"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish9", Price: "900"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish10", Price: "1000"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish11", Price: "1100"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish12", Price: "1200"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish13", Price: "1300"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish14", Price: "1400"},
+			{ImageSrc:"holder.js/50x50", Name: "Dish15", Price: "1500"},
+		];
+	}
+})(jQuery);
